6441f6709c2d9a37eaafb40858a90f32a3874d7c
green4837 <green4837@naver.com>
2021-11-28 04:34:13 +0900

    55

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Body />
+  <Footer />
+</template>
+
+<script>
+import Body from './components/Body.vue'
+import Footer from './components/common/Footer.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Body
+    , Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer>
+        <p>Copyright © 2018. WEMADE TREE PTE. LTD. All rights reserved.</p>
+    </footer>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+    footer{ z-index: 1;
+    width: calc(100% - 40px);
+    height: 75px;
+    line-height: 75px;
+    display: flex;
+    justify-content: center;
+    background: #000;
+    color: #fff;
+    font-size: 12px;
+    padding: 0 20px
+}
+</style>
